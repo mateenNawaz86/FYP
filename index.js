afa83b8a5f4affe33790d29f97ae708ab43021d9
Mateen Mirani <mateenmirani125@gmail.com>
2022-11-03 11:11:45 +0500

    initial setup

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import express from "express";
-import { router as userRoute } from "./routes/User.js";
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// routes
-app.use("/", userRoute);
-
 app.listen(port, () => {
-  console.log("MVC app is listening port", port);
+  console.log(`FYP server listening on port ${port}`);
 });
--- routes/User.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import userController from "../controller/UserController.js";
-
-// Route 1. Create a user
-router.post("/user", userController.post);
-
-export { router };
